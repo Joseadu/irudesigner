013be24f79862d9e5c72358f3820c49e30d77eb4
Jose Antonio Diaz Urbano <joseantonio.diazurbano@colaborador.elcorteingles.es>
2025-07-08 14:40:34 +0200

    barkers, deporte-mujer, via-crucis, stella-bikes

--- src/app/pages/festival-via-crucis/festival-via-crucis.component.ts
+++ src/app/pages/festival-via-crucis/festival-via-crucis.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DetailComponentBase } from '../../core/base/detail-component-base';
 import { CommonModule } from '@angular/common';
 
@@ -8,6 +8,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './festival-via-crucis.component.html',
   styleUrl: './festival-via-crucis.component.css'
 })
-export class FestivalViaCrucisComponent extends DetailComponentBase {
+export class FestivalViaCrucisComponent extends DetailComponentBase implements OnInit {
 
 }
