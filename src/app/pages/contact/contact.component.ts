9bdebab5710c520617ee8d2e4e298477193a3c08
Jose Diaz <joseantonio@mydance.zone>
2025-09-07 00:44:00 +0200

    contacto

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ThemeService } from '../../core/services/theme.service';
+import { ServicesItemsService } from '../about/services/services-items.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit, OnDestroy {
+  qhItems: any[] = [];
 
+  constructor(private themeService: ThemeService, private serviesItems: ServicesItemsService) {}
+
+  ngOnInit() {
+    this.themeService.setTheme('#D7ECED', '#114653');
+    document.body.style.background = '#D7ECED';
+    // Cargar los items de servicios
+    this.qhItems = this.serviesItems.qhItems;
+  }
+
+  ngOnDestroy(): void {
+    this.themeService.setTheme('#F1F7F8', '#114653');
+    document.body.style.background = '#F1F7F8';
+  }
 }
