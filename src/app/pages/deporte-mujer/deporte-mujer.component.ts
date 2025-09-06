0e0ba4134573d87a70afc688be285cee83a22171
Jose Diaz <joseantonio@mydance.zone>
2025-09-06 19:14:34 +0200

    polishing deporte-mujer

--- src/app/pages/deporte-mujer/deporte-mujer.component.ts
+++ src/app/pages/deporte-mujer/deporte-mujer.component.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { DetailComponentBase } from '../../core/base/detail-component-base';
+import { SliderSingleComponent } from '../../shared/slider-single/slider-single.component';
 
 @Component({
   selector: 'app-deporte-mujer',
-  imports: [CommonModule],
+  imports: [CommonModule, SliderSingleComponent],
   templateUrl: './deporte-mujer.component.html',
   styleUrl: './deporte-mujer.component.css'
 })
 export class DeporteMujerComponent extends DetailComponentBase implements OnInit {
-
+  deporteMujerImages = [
+    'assets/images/deporte-mujer/+dep +mujer 3.jpg',
+    'assets/images/deporte-mujer/+dep +mujer calería 2.jpg',
+    'assets/images/deporte-mujer/+dep +mujer 7.jpg',
+  ];
 }
