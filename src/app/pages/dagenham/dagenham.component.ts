651468f90674721603ced31dbcfe4ca0b850ebbb
Jose Diaz <joseantonio@mydance.zone>
2025-09-16 21:13:35 +0200

    recommended dagenham

--- src/app/pages/dagenham/dagenham.component.ts
+++ src/app/pages/dagenham/dagenham.component.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { DetailComponentBase } from '../../core/base/detail-component-base';
 import { SliderSingleComponent } from '../../shared/slider-single/slider-single.component';
+import { RecommendedCardsComponent } from '../../shared/recommended-cards/recommended-cards.component';
 
 @Component({
   selector: 'app-dagenham',
   imports: [
     CommonModule,
-    SliderSingleComponent
+    SliderSingleComponent,
+    RecommendedCardsComponent
   ],
   templateUrl: './dagenham.component.html',
   styleUrl: './dagenham.component.css'
@@ -17,5 +19,26 @@ export class DagenhamComponent extends DetailComponentBase implements OnInit {
     'assets/images/dagenham/cartel 1.jpg',
     'assets/images/dagenham/cartel 2.jpg',
     'assets/images/dagenham/cartel 3.jpg',
+  ];
+
+  recommendations = [
+    {
+      title: 'CONCILIO COMMUNICATIONS',
+      description: 'Rebranding y Diseño UX/UI',
+      imageUrl: 'assets/images/concilio/concilio.jpg',
+      url: 'concilio'
+    },
+    {
+      title: 'FORMER TOOTING POLICE STATION',
+      description: 'Identidad Visual',
+      imageUrl: 'assets/images/tooting-police-station/tooting.jpg',
+      url: 'tooting-police-station'
+    },
+    {
+      title: 'CLARITY AI',
+      description: 'Rebranding, diseño UI/UX, diseño gráfico, diseño editorial y motion graphics',
+      imageUrl: 'assets/images/clarity-ai/cover.jpg',
+      url: 'clarity-ai'
+    }
   ];
 }
