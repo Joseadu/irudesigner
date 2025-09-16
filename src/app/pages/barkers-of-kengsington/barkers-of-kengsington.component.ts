d016c67162a91d69029222cf97f10a8b81417216
Jose Diaz <joseantonio@mydance.zone>
2025-09-16 21:15:24 +0200

    recommended barkers

--- src/app/pages/barkers-of-kengsington/barkers-of-kengsington.component.ts
+++ src/app/pages/barkers-of-kengsington/barkers-of-kengsington.component.ts
@@ -1,13 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { DetailComponentBase } from '../../core/base/detail-component-base';
 import { CommonModule } from '@angular/common';
+import { RecommendedCardsComponent } from '../../shared/recommended-cards/recommended-cards.component';
 
 @Component({
   selector: 'app-barkers-of-kengsington',
-  imports: [CommonModule],
+  imports: [CommonModule, RecommendedCardsComponent],
   templateUrl: './barkers-of-kengsington.component.html',
   styleUrl: './barkers-of-kengsington.component.css',
 })
-export class BarkersOfKengsingtonComponent
-  extends DetailComponentBase
-  implements OnInit {}
+export class BarkersOfKengsingtonComponent extends DetailComponentBase implements OnInit {
+
+
+  recommendations = [
+    {
+      title: 'DAGENHAM GREEN',
+      description: 'Diseño gráfico y diseño UI/UX',
+      imageUrl: 'assets/images/dagenham/dagenham.jpg',
+      url: 'dagenham'
+    },
+    {
+      title: 'FORMER TOOTING POLICE STATION',
+      description: 'Identidad Visual',
+      imageUrl: 'assets/images/tooting-police-station/tooting.jpg',
+      url: 'tooting-police-station'
+    },
+    {
+      title: 'CONCILIO COMMUNICATIONS',
+      description: 'Rebranding y Diseño UX/UI',
+      imageUrl: 'assets/images/concilio/concilio.jpg',
+      url: 'concilio'
+    }
+  ];
+
+}
