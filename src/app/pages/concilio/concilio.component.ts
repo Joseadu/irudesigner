60e9142c7bdfe0deccc4b2fc258550795e24be31
Jose Diaz <joseantonio@mydance.zone>
2025-09-16 21:10:47 +0200

    recommended concilio

--- src/app/pages/concilio/concilio.component.ts
+++ src/app/pages/concilio/concilio.component.ts
@@ -1,11 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { DetailComponentBase } from '../../core/base/detail-component-base';
+import { RecommendedCardsComponent } from '../../shared/recommended-cards/recommended-cards.component';
 
 @Component({
   selector: 'app-concilio',
-  imports: [CommonModule],
+  imports: [CommonModule, RecommendedCardsComponent],
   templateUrl: './concilio.component.html',
   styleUrl: './concilio.component.css',
 })
-export class ConcilioComponent extends DetailComponentBase implements OnInit {}
+export class ConcilioComponent extends DetailComponentBase implements OnInit {
+
+  recommendations = [
+    {
+      title: 'DAGENHAM GREEN',
+      description: 'Diseño gráfico y diseño UI/UX',
+      imageUrl: 'assets/images/dagenham/dagenham.jpg',
+      url: 'dagenham'
+    },
+    {
+      title: 'FORMER TOOTING POLICE STATION',
+      description: 'Identidad Visual',
+      imageUrl: 'assets/images/tooting-police-station/tooting.jpg',
+      url: 'tooting-police-station'
+    },
+    {
+      title: 'BARKERS OF KENSINGTON BUILDING',
+      description: 'Identidad Visual',
+      imageUrl: 'assets/images/barkers/barkers-600.jpg',
+      url: 'barkers-of-kensington'
+    }
+  ];
+
+}
