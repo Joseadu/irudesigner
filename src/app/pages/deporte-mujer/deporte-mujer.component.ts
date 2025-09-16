f9372920433d29ea6e85bc36deb572a90f2c2f1c
Jose Diaz <joseantonio@mydance.zone>
2025-09-16 21:17:25 +0200

    recommended deporte-mujer

--- src/app/pages/deporte-mujer/deporte-mujer.component.ts
+++ src/app/pages/deporte-mujer/deporte-mujer.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { DetailComponentBase } from '../../core/base/detail-component-base';
 import { SliderSingleComponent } from '../../shared/slider-single/slider-single.component';
+import { RecommendedCardsComponent } from '../../shared/recommended-cards/recommended-cards.component';
 
 @Component({
   selector: 'app-deporte-mujer',
-  imports: [CommonModule, SliderSingleComponent],
+  imports: [CommonModule, SliderSingleComponent, RecommendedCardsComponent],
   templateUrl: './deporte-mujer.component.html',
   styleUrl: './deporte-mujer.component.css'
 })
@@ -14,5 +15,25 @@ export class DeporteMujerComponent extends DetailComponentBase implements OnInit
     'assets/images/deporte-mujer/+dep +mujer 3.jpg',
     'assets/images/deporte-mujer/+dep +mujer calería 2.jpg',
     'assets/images/deporte-mujer/+dep +mujer 7.jpg',
+  ];
+  recommendations = [
+    {
+      title: 'FORMER TOOTING POLICE STATION',
+      description: 'Identidad Visual',
+      imageUrl: 'assets/images/tooting-police-station/tooting.jpg',
+      url: 'tooting-police-station'
+    },
+    {
+      title: 'CLARITY AI',
+      description: 'Rebranding, diseño UI/UX, diseño gráfico, diseño editorial y motion graphics',
+      imageUrl: 'assets/images/clarity-ai/cover.jpg',
+      url: 'clarity-ai'
+    },
+    {
+      title: 'CONCILIO COMMUNICATIONS',
+      description: 'Rebranding y Diseño UX/UI',
+      imageUrl: 'assets/images/concilio/concilio.jpg',
+      url: 'concilio'
+    }
   ];
 }
