4b37b50c60a528c67a85c825ec09e48089fc72bf
Jose Diaz <joseantonio@mydance.zone>
2025-09-16 21:22:06 +0200

    recommended stella-bijes

--- src/app/pages/stella-bikes/stella-bikes.component.ts
+++ src/app/pages/stella-bikes/stella-bikes.component.ts
@@ -1,13 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { DetailComponentBase } from '../../core/base/detail-component-base';
+import { RecommendedCardsComponent } from '../../shared/recommended-cards/recommended-cards.component';
 
 @Component({
   selector: 'app-stella-bikes',
-  imports: [CommonModule],
+  imports: [CommonModule, RecommendedCardsComponent],
   templateUrl: './stella-bikes.component.html',
   styleUrl: './stella-bikes.component.css'
 })
 export class StellaBikesComponent extends DetailComponentBase {
 
+  recommendations = [
+    {
+      title: 'FORMER TOOTING POLICE STATION',
+      description: 'Identidad Visual',
+      imageUrl: 'assets/images/tooting-police-station/tooting.jpg',
+      url: 'tooting-police-station'
+    },
+    {
+      title: 'HOTEL TEATRO ALAMEDA MÁLAGA',
+      description: 'Branding',
+      imageUrl: 'assets/images/hotel-alameda/portada.png',
+      url: 'hotel-alameda'
+    },
+    {
+      title: 'CLARITY AI',
+      description: 'Rebranding, diseño UI/UX, diseño gráfico, diseño editorial y motion graphics',
+      imageUrl: 'assets/images/clarity-ai/cover.jpg',
+      url: 'clarity-ai'
+    },
+  ];
 }
