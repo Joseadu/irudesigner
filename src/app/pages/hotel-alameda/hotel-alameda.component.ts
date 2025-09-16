3ccd6acfd40e78c84fe66e93569f096b9858a55b
Jose Diaz <joseantonio@mydance.zone>
2025-09-16 21:32:32 +0200

    recommended hotel-alameda

--- src/app/pages/hotel-alameda/hotel-alameda.component.ts
+++ src/app/pages/hotel-alameda/hotel-alameda.component.ts
@@ -1,13 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { DetailComponentBase } from '../../core/base/detail-component-base';
+import { RecommendedCardsComponent } from '../../shared/recommended-cards/recommended-cards.component';
 
 @Component({
   selector: 'app-hotel-alameda',
-  imports: [CommonModule],
+  imports: [CommonModule, RecommendedCardsComponent],
   templateUrl: './hotel-alameda.component.html',
   styleUrl: './hotel-alameda.component.css'
 })
 export class HotelAlamedaComponent extends DetailComponentBase {
+
+  recommendations = [
+    {
+      title: 'DAGENHAM GREEN',
+      description: 'Diseño gráfico y diseño UI/UX',
+      imageUrl: 'assets/images/dagenham/dagenham.jpg',
+      url: 'dagenham'
+    },
+    {
+      title: 'CLARITY AI',
+      description: 'Rebranding, diseño UI/UX, diseño gráfico, diseño editorial y motion graphics',
+      imageUrl: 'assets/images/clarity-ai/cover.jpg',
+      url: 'clarity-ai'
+    },
+    {
+      title: '+DEPORTE +MUJER DIPUTACIÓN DE MÁLAGA',
+      description: 'Identidad Visual, diseño gráfico, video, fotografía e impresión digital',
+      imageUrl: 'assets/images/deporte-mujer/pancarta-grande.png',
+      url: 'deporte-mujer'
+    },
+  ];
 
 }
--- src/app/pages/restaurante-yaki/restaurante-yaki.component.ts
+++ src/app/pages/restaurante-yaki/restaurante-yaki.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { DetailComponentBase } from '../../core/base/detail-component-base';
 import { CommonModule } from '@angular/common';
+import { RecommendedCardsComponent } from '../../shared/recommended-cards/recommended-cards.component';
 
 @Component({
   selector: 'app-restaurante-yaki',
-  imports: [CommonModule],
+  imports: [CommonModule, RecommendedCardsComponent],
   templateUrl: './restaurante-yaki.component.html',
   styleUrl: './restaurante-yaki.component.css'
 })
 export class RestauranteYakiComponent extends DetailComponentBase {
+
+  recommendations = [
+    {
+      title: 'DAGENHAM GREEN',
+      description: 'Diseño gráfico y diseño UI/UX',
+      imageUrl: 'assets/images/dagenham/dagenham.jpg',
+      url: 'dagenham'
+    },
+    {
+      title: 'HOTEL TEATRO ALAMEDA MÁLAGA',
+      description: 'Branding',
+      imageUrl: 'assets/images/hotel-alameda/portada.png',
+      url: 'hotel-alameda'
+    },
+    {
+      title: 'WEEKEND FESTIVAL',
+      description: 'Branding',
+      imageUrl: 'assets/images/weekend/iPhone XR Isometric.jpg',
+      url: 'weekend-festival'
+    },
+  ];
 
 }
