7994a501f698bfec3c5d523484a6dd12dc6ca69b
Jose Diaz <joseantonio@mydance.zone>
2025-09-16 21:28:21 +0200

    recommended via-crucis

--- src/app/pages/festival-via-crucis/festival-via-crucis.component.ts
+++ src/app/pages/festival-via-crucis/festival-via-crucis.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { DetailComponentBase } from '../../core/base/detail-component-base';
 import { CommonModule } from '@angular/common';
+import { RecommendedCardsComponent } from '../../shared/recommended-cards/recommended-cards.component';
 
 @Component({
   selector: 'app-festival-via-crucis',
-  imports: [CommonModule],
+  imports: [CommonModule, RecommendedCardsComponent],
   templateUrl: './festival-via-crucis.component.html',
   styleUrl: './festival-via-crucis.component.css'
 })
 export class FestivalViaCrucisComponent extends DetailComponentBase implements OnInit {
 
+  recommendations = [
+    {
+      title: 'WEEKEND FESTIVAL',
+      description: 'Branding',
+      imageUrl: 'assets/images/weekend/iPhone XR Isometric.jpg',
+      url: 'weekend-festival'
+    },
+    {
+      title: 'HOTEL TEATRO ALAMEDA MÁLAGA',
+      description: 'Branding',
+      imageUrl: 'assets/images/hotel-alameda/portada.png',
+      url: 'hotel-alameda'
+    },
+    {
+      title: 'CLARITY AI',
+      description: 'Rebranding, diseño UI/UX, diseño gráfico, diseño editorial y motion graphics',
+      imageUrl: 'assets/images/clarity-ai/cover.jpg',
+      url: 'clarity-ai'
+    },
+  ];
 }
